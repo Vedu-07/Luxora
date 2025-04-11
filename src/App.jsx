3759f13b6cc4ecc84bf59074912b92e5eb2eb640
Vedu-07 <vedantswami02@gmail.com>
2025-04-11 09:48:55 +0530

    Portfolio Section Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Services from "./components/Services"
-
+import Portfolio from "./components/Portfolio"
 function App() {
   return (
     <main className='overflow-x-hidden antialiased text-neutral-800 '>
         <Navbar/>
         <Hero/>
         <Services/>
+        <Portfolio/>
     </main>
   )
 }
